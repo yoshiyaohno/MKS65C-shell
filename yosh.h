c2a2daf6292489c63a371fd3ca11e5b828e4703b
Yoshiya Ohno <yohno@stuy.edu>
2018-11-19 15:36:02 -0500

    inital just all the standard library stuff

---
+++ yosh.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+char **parse_args(char *line);
