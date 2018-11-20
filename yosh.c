df5b62b24eb0e257cdb8746e7d5c7dfc9c2ff0a2
Yoshiya Ohno <yohno@stuy.edu>
2018-11-20 15:38:19 -0500

    separation actually worjks

--- yosh.c
+++ yosh.c
@@ -1,17 +1,36 @@
 #include "yosh.h"
 
-char **parse_args( char *line )
+#define MAX_ARGS 16
+#define MAX_LINE 256
+
+char **_split( char *line, char *delims, int maxlen)
 {
-    char **args = calloc(8, sizeof(char *)); 
+    char **args = calloc(maxlen, sizeof(char *)); 
     int i = 0;
-    while( line ) {
-        args[i] = strsep( &line, " " );
-        i++;
-    }
+    while(args[i++] = strsep(&line, delims));
     return args;
+}
+
+char **parse_args(char *line)
+{
+    _split(line, " ", MAX_ARGS);
 }
 
 int main(int argc, char *argv)
 {
+    char line[MAX_LINE];
+    char **args;
+
+    printf(">");
+    fgets(line, MAX_LINE, stdin);
+    line[strlen(line)-1] = '\0';
+    args = parse_args(line);
+
+    int i = 0;
+    while(args[i])
+        printf("\"%s\"\n", args[i++]);
+
+    free(args);
+
     return 0;
 }
