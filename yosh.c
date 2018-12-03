ca50af73c75be6788bf8a0afc422dce6e2a50b43
Yoshiya <yohno@stuy.edu>
2018-12-02 21:24:52 -0500

    working > redirection

--- yosh.c
+++ yosh.c
@@ -1,6 +1,6 @@
 #include "yosh.h"
 
-#define MAX_LINE 256
+#define MAX_LINE 512
 
 char *prompt_in(char *buf)
 {
@@ -10,18 +10,62 @@ char *prompt_in(char *buf)
     return buf;
 }
 
-void run_cmd(char **args)
+int run_cmd(char **args)
 {
-    execvp(args[0], args);
-    // only continues if no error
-    perror(args[0]);
-    exit(1);
+    int child, status;
+    if(!strcmp("exit", args[0])) 
+        exit(0);
+    if((child = fork()) == 0) {
+        execvp(args[0], args);
+        // only continues if no error
+        perror(args[0]);
+        exit(1);
+    }
+    waitpid(child, &status, 0);
+    return status;
+}
+
+void remove_args(char **args, int from, int count)
+{
+    while(args[from] = args[count + from++]);
+}
+
+void process_redirects(char **args)
+{
+    int i = 0;
+    int sav_stdin  = dup(0);
+    int sav_stdout = dup(1);
+    int sav_stderr = dup(2);
+    int file;
+    while(args[i]) {
+        if(!strcmp(args[i], ">")) {
+            //printf("args[i]: %s\nargs[i+1]: %s\n", args[i], args[i+1]);
+            file = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC);
+            if(file == -1) {
+                perror("\">\"");
+                return;
+            }
+            dup2(file, 1);
+            remove_args(args, i, 2);
+        }
+        else
+            ++i;
+    }
+
+    int status = run_cmd(args);
+
+    dup2(sav_stdin,  0);
+    dup2(sav_stdout, 1);
+    dup2(sav_stderr, 2);
+    close(sav_stdin);
+    close(sav_stdout);
+    close(sav_stderr);
 }
 
 int main(int argc, char *argv)
 {
     char line[MAX_LINE];
-    char **args, **cmds, **jef;
+    char **args, **cmds;
     int child, status;
     int i = 0;
 
@@ -31,20 +75,15 @@ int main(int argc, char *argv)
         i = -1;
         while(cmds[++i]) {
             args = parse_args(cmds[i]);
-            
             // printf("args:\n");
             // char **oof = args;
             // while(*oof)
             //     printf("<%s>\n", *oof++);
             // printf("done args\n\n");
-
             if(!args[0]) continue;
 
-            if(!strcmp("exit", args[0])) 
-                exit(0);
-            if((child = fork()) == 0)
-                run_cmd(args);
-            waitpid(child, &status, 0);
+            process_redirects(args);
+
             free(args);
         }
 
